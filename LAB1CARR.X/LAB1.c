424c9dc5b04d08f3d4268a82b145dfaaeb94befb
CAVENDANO17192 <60333304+CAVENDANO17192@users.noreply.github.com>
2020-01-26 21:20:23 -0600

    LAB

---
+++ LAB1CARR.X/LAB1.c
@@ -0,0 +1,13 @@
+/*
+ * File:   LAB1.c
+ * Author: CHARLIE
+ *
+ * Created on 26 de enero de 2020, 21:10
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ LAB1CARR.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=LAB1CARR.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/LAB1CARR.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=lab1carr.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/lab1carr.x.tar
